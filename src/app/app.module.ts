aba79b8ba3ccb46409cbf38bedc99cbd775248cc
Riccardo Petralia <petraliariccardo@gmail.com>
2018-03-25 11:48:56 +0200

    primo commint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { ClarityModule } from '@clr/angular';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 
 import { AppComponent } from './app.component';
+import {EventsComponent} from './events/events.component';
+import { EventComponent } from './events/event.componet';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, EventsComponent, EventComponent, NavbarComponent, CalendarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ClarityModule.forRoot(), FormsModule, BrowserAnimationsModule, HttpClientModule, HttpModule, JsonpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: EventsComponent
+      },
+      {
+        path: 'calendar',
+        component: CalendarComponent
+      }
+    ]),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/events/events.component.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit, AfterViewInit, OnChanges, SimpleChange, Input, QueryList, ViewChildren, ElementRef} from '@angular/core';
+import { Event } from './events.model';
+import { EventsService } from './events.service';
+import { ClrDatagridStateInterface, ClrDatagrid } from '@clr/angular';
+import { EventComponent } from './event.componet';
+import { HttpClient } from '@angular/common/http/src/client';
+
+
+
+
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-events',
+    templateUrl: 'events.component.html',
+    styleUrls: ['events.component.css'],
+    providers: [EventsService]
+})
+
+export class EventsComponent implements OnChanges {
+
+    eventsList: Event[];
+    selected: Event[] = [];
+    copySelectedEvent: Event;
+    loading = true;
+    total: number;
+    state: ClrDatagridStateInterface;
+    formVisible: boolean;
+    createFLag: boolean;
+    myElem: any;
+
+
+    constructor(private service: EventsService) {}
+
+      ngOnChanges () {
+      }
+
+    refresh(state: ClrDatagridStateInterface) {
+        this.state = state;
+        this.loading = true;
+        const filters: { [prop: string]: any[] } = {};
+        if (state.filters) {
+            for (const filter of state.filters) {
+                const { property, value } = <{ property: string, value: string }>filter;
+                filters[property] = [value];
+            }
+        }
+        this.service.filter(filters)
+        .sort(<{ by: string, reverse: boolean }>state.sort)
+        .fetch(state.page.from, state.page.size)
+        .sendRequest().subscribe(arg => {this.eventsList = arg.body; this.total = parseInt(arg.headers.get('X-Total-Count'), 10); });
+
+        this.selected.splice(0, this.selected.length);
+    }
+
+    delete() {
+
+        this.service.deleteEvents(this.selected).subscribe(() => {
+            this.selected.splice(0, 1);
+            if (this.selected.length > 0) {
+                this.delete();
+            }else {
+                this.refresh(this.state);
+              } });
+
+    }
+
+    edit() {
+        this.createFLag = false;
+        this.copySelectedEvent = this.selected[0];
+        this.showForm();
+    }
+
+    create() {
+
+        const event = new Event;
+        this.copySelectedEvent = event;
+        this.createFLag = true;
+        this.showForm();
+    }
+
+    save() {
+
+        if ( this.createFLag ) {
+            console.log(this.copySelectedEvent);
+            this.service.createEvent(this.copySelectedEvent).subscribe(arg => {this.refresh(this.state); this.hideForm(); } ) ;
+        } else { this.service.updateEvent(this.selected[0]).subscribe(arg => {this.refresh(this.state);
+                                                                              this.hideForm(); this.selected.splice(0, 1); } ) ; }
+    }
+
+    showForm() { this.formVisible = true; }
+    hideForm() { this.formVisible = false; }
+
+}
+
+
+
